a041805744e6580eb7cfa494867cd271a369b385
seoYee <lee.seoyee9011@gmail.com>
2017-02-17 18:35:26 +0900

    seoyee tetris 1

--- app/src/main/java/com/test/testris/MainActivity.java
+++ app/src/main/java/com/test/testris/MainActivity.java
@@ -4,98 +4,145 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private int[][] mGameBoard = new int[18][12];
+
+    private static final int[][] iBlock1 = new int[][]{
+
+            {1, 1, 1, 1},
+            {0, 0, 0, 0}
+    };
+    private int[][] iBlock2 = new int[][]{
+            {1, 0},
+            {1, 0},
+            {1, 0},
+            {1, 0}
+
+    };
+
+    private int[][] zBlock1 = new int[][]{
+            {2, 2, 0},
+            {0, 2, 2}
+    };
+
+    private int[][] zBlock2 = new int[][]{
+            {0, 2},
+            {2, 2},
+            {2, 0}
+    };
+
+    private int[][] sBlock1 = new int[][]{
+            {0, 3, 3},
+            {3, 3, 0}
+    };
+
+    private int[][] sBlock2 = new int[][]{
+            {3, 0},
+            {3, 3},
+            {0, 3}
+    };
+
+    private int[][] lBlock1 = new int[][]{
+            {0, 0, 4},
+            {4, 4, 4}
+    };
+
+    private int[][] lBlock2 = new int[][]{
+            {4, 4},
+            {0, 4},
+            {0, 4}
+    };
+
+    private int[][] lBlock3 = new int[][]{
+            {4, 4, 4},
+            {4, 0, 0}
+    };
+
+    private int[][] lBlock4 = new int[][]{
+            {4, 0},
+            {4, 0},
+            {4, 4}
+    };
+
+    private int[][] jBlock1 = new int[][]{
+            {5, 0, 0},
+            {5, 5, 5}
+    };
+
+    private int[][] jBlock2 = new int[][]{
+            {0, 5},
+            {0, 5},
+            {5, 5}
+    };
+
+    private int[][] jBlock3 = new int[][]{
+            {5, 5, 5},
+            {0, 0, 5}
+    };
+
+    private int[][] jBlock4 = new int[][]{
+            {5, 5},
+            {5, 0},
+            {5, 0}
+    };
+
+    private int[][] tBlock1 = new int[][]{
+            {6, 6, 6},
+            {0, 6, 0}
+    };
+
+    private int[][] tBlock2 = new int[][]{
+            {6, 0},
+            {6, 6},
+            {6, 0}
+    };
+
+    private int[][] tBlock3 = new int[][]{
+            {0, 6, 0},
+            {6, 6, 6}
+    };
+
+    private int[][] tBlock4 = new int[][]{
+            {0, 6},
+            {6, 6},
+            {0, 6}
+    };
+
+    private int[][] oBlock = new int[][]{
+            {7, 7},
+            {7, 7}
+    };
+
+    private int[][][] blocks = {
+            iBlock1,
+            sBlock1,
+            jBlock1,
+            lBlock1,
+            tBlock1,
+            oBlock,
+            zBlock1,
+            zBlock2
+    };
+
+    private int[][] mGameBoard = new int[18][12]; //18행 12열짜리 이중배열 0으로 초기화되어 있음
     TetrisView tetrisView;
     Random random = new Random();
     private TimerTask mTask;
     private Timer mTimer;
+    private int row = mGameBoard.length; //블록의 좌상단 처음 시작하는 부분
+    private int col = 4;
+    private int[][] curBlock;
 
-
-    //***************************//
-    //여기서 시작
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        tetrisView = (TetrisView) findViewById(R.id.tetrisView);
-        tetrisView.setGameBoard(mGameBoard);
-
-    }
-
-    @Override
-    protected void onPause() {
-        stopTimer();
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        startTimer(1000);
-        super.onResume();
-    }
-
-    @Override
-    //***************************//
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.buttonL: //Left
-                for (int i = 0; i < mGameBoard[0].length; i++) {
-                    mGameBoard[4][i] = random.nextInt(7) + 1;
-                }
-                break;
-            case R.id.buttonR: //Right
-                for (int i = 0; i < mGameBoard[0].length; i++) {
-                    mGameBoard[0][i] = random.nextInt(7) + 1;
-                }
-                break;
-            case R.id.buttonU: //Up
-                tetrisView.setMoveBlock(2, 13, new int[][]{
-                        {0, 1, 0},
-                        {1, 1, 1}
-                });
-                break;
-            case R.id.buttonD: //Down
-                tetrisView.setMoveBlock(5, 13, new int[][]{
-                        {3, 3, 0},
-                        {0, 3, 3}
-                });
-                break;
-            case R.id.buttonEnter:
-
-                tetrisView.setMoveBlock(0, 0, null);
-                break;
-
-        }
-        tetrisView.invalidate();
-    }
-
-
-    //***************************//
-    // 타이머
-    private void onTimer() {
-        if (test > 0) {
-            test--;
-        }
-        tetrisView.setMoveBlock(4, test, testBlock);
-        tetrisView.invalidate();
-    }
-
-    private int test;
-    private int[][] testBlock = new int[][]{
-            {4, 0, 4},
-            {4, 4, 4}
-    };
 
     private void startTimer(int period) {
         stopTimer();
-
-        test = mGameBoard.length;
-
+        //row = mGameBoard.length;
         mTask = new TimerTask() {
             @Override
             public void run() {
@@ -119,4 +166,195 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             mTimer = null;
         }
     }
+
+    private boolean checkBlow() {
+        boolean check = true;
+        for (int i = 0; i < curBlock[0].length; i++) {
+            if (curBlock[0][i] * mGameBoard[row - 1][col + i] != 0) {
+                check = false;
+            }
+        }
+        return check;
+    }
+
+    private boolean checkLeft() {
+        boolean check = true;
+        for (int i = 0; i < curBlock.length; i++) {
+                if (curBlock[i][0] * mGameBoard[row+i][col] != 0) {
+                    check = false;
+                }
+        }
+        return check;
+    }
+
+    private boolean checkRight(){
+        boolean check = true;
+        for(int i = 0; i < curBlock.length; i++){
+            if(curBlock[i][curBlock[0].length-1] * mGameBoard[row][col+curBlock[0].length] !=0){
+                check = false;
+            }
+        }
+        return check;
+    }
+
+    private void onTimer() {
+        if (row > 0) {
+            if(checkBlow()){
+                row--;
+            }else{
+                stitchBlock();
+                setRandomBlock();
+                row = mGameBoard.length;
+                col = 4;
+            }
+        }else{
+            stitchBlock();
+            setRandomBlock();
+            row = mGameBoard.length;
+            col = 4;
+        }
+        tetrisView.setMoveBlock(col, row, curBlock);
+        tetrisView.invalidate();
+    }
+
+    private void setRandomBlock() {
+        int i = random.nextInt(7);
+        curBlock = blocks[i];
+    }
+
+    private void stitchBlock() {
+        for (int i = 0; i < curBlock.length; i++) {
+            for (int j = 0; j < curBlock[i].length; j++) {
+                if (mGameBoard[row + i][col + j] == 0) {
+                    mGameBoard[row + i][col + j] = curBlock[i][j];
+                }
+            }
+        }
+        curBlock = null;
+    }
+
+    //
+    //***************************//
+    //여기서 시작
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tetrisView = (TetrisView) findViewById(R.id.tetrisView);
+        tetrisView.setGameBoard(mGameBoard);
+    }
+
+    @Override
+    protected void onResume() {
+        if (curBlock == null) {
+            setRandomBlock();
+        }
+        startTimer(1000);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        stopTimer();
+        super.onPause();
+    }
+
+    @Override
+    //***************************//
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonL: //Left
+                if (col > 0) {
+                    if(checkLeft()){
+                        col--;
+                        tetrisView.setMoveBlock(col, row, curBlock);
+                    }else{
+                        tetrisView.setMoveBlock(col, row, curBlock);
+                    }
+                }
+                break;
+            case R.id.buttonR: //Right
+                if (col + curBlock[0].length < 12) {
+                    if (checkRight()) {
+                        col++;
+                        tetrisView.setMoveBlock(col,row, curBlock);
+                    } else {
+                        tetrisView.setMoveBlock(col, row, curBlock);
+                    }
+                }
+                break;
+
+            case R.id.buttonU: //Up
+///////////////////////////////////////////
+                break;
+
+            case R.id.buttonD: //Down
+                tetrisView.setMoveBlock(5, 13, new int[][]{
+                        {3, 3, 0},
+                        {0, 3, 3}
+                });
+                break;
+
+            case R.id.buttonEnter:
+                if (mTimer != null) {
+                    onPause();
+                } else {
+                    onResume();
+                }
+                break;
+        }
+        tetrisView.invalidate();
+    }
+
+
+
+    //***************************//
+    // 타이머
+
+
+
+
+//    private void checkBoard(int [][] curBlock){
+//        int r = curBlock.length;
+//        int c = curBlock[r].length;
+//
+//        for(int i = 0; i < r; i++) {
+//            if(curBlock[r][i]!=0 && mGameBoard)
+//        }
+//    }
+
+
+
+ //   private boolean checkBoard() {
+//        boolean check = true;
+//
+//            for (int i = 0; i < curBlock.length; i++) {
+//                for (int j = 0; j < curBlock[0].length; j++) {
+//                    if (curBlock[i][0] != 0 && col > 0) {
+//                        if (mGameBoard[row + i][col - 1] != 0) {
+//                            check = false;
+//                        }
+//                    } else if (curBlock[curBlock.length-1][j] != 0) {
+//                        if (mGameBoard[row + curBlock.length][col + j] != 0) {
+//                            check = false;
+//                        }
+//                    } else if (curBlock[i][col] !=0 && col < mGameBoard.length){
+//                        if (mGameBoard[row+i][col+curBlock[0].length]!=0){
+//                            check = false;
+//                        }
+//                    }
+//                }
+//            }
+//        return check;
+//    }
+
+
+//                    if(row-1>=0){
+//                        if (curBlock[i][j] * mGameBoard[row - 1 + i][col + j] != 0) {
+//                            stitchBlock();
+//                            setRandomBlock();
+//                            row = mGameBoard.length;
+//                            col = 4;
+//                        }
+//                    }
 }
--- app/src/main/java/com/test/testris/TetrisView.java
+++ app/src/main/java/com/test/testris/TetrisView.java
@@ -4,14 +4,9 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.ColorFilter;
 import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffColorFilter;
 import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.view.View;
@@ -23,6 +18,7 @@ import java.util.Random;
  */
 
 public class TetrisView extends View {
+
     private int[][] mGameBoard;
 
     private Bitmap mBlock[] = new Bitmap[7];
@@ -31,25 +27,27 @@ public class TetrisView extends View {
     private RectF rectF = new RectF();
     private int mMovingBlockX = 0;
     private int mMovingBlockY = 0;
-    private int[][] mMovingSahpe = null;
+    private int[][] mMovingShape = null;
+
 
 
     public TetrisView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics(); //스크린 사이즈 가져오기
 
-        density = metrics.density;
+        density = metrics.density; //160dpi screen this density value will be 1.
         paintBorder.setStyle(Paint.Style.STROKE);
         paintBorder.setColor(Color.argb(120, 0, 0, 0));
         paintBorder.setStrokeWidth(3 * density);
 
         BitmapDrawable bitmapDrawable = (BitmapDrawable) context.getResources().getDrawable(R.drawable.block_base);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-
-        }
+      //  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {}
+        //직사각형 가로세로 64짜리를 하나 만들어둠
         RectF rectF1 = new RectF(0, 0, 64, 64);
+        //Paint 객체도 하나 만들어둠.
         Paint p1 = new Paint();
+        //xml에 설정해둔 색깔의 int 숫자를 colors 배열에 담음. 블록이 7개이므로 7색
         int[] colors = new int[]{
                 getResources().getColor(R.color.block1),
                 getResources().getColor(R.color.block2),
@@ -59,12 +57,19 @@ public class TetrisView extends View {
                 getResources().getColor(R.color.block6),
                 getResources().getColor(R.color.block7)
         };
+        //설정색을 가진 RoundRect블록 하나를 그림
         for (int i = 0; i < colors.length; i++) {
-            Bitmap bm = Bitmap.createBitmap(64, 64, Bitmap.Config.ARGB_8888);
+            //가로세로 64짜리 bitmap객체 bm 만든다
+            Bitmap bm = Bitmap.createBitmap(64, 64, Bitmap.Config.ARGB_8888);//(블록 한칸의 가로, 세로, 해상도?설정)
+            //Canvas 객체 c에 위에서 만든 bm을 붙인다.
             Canvas c = new Canvas(bm);
+            //위에서 만든 p1 객체의 색깔을 colors 배열에 담긴 색으로 지정한다
             p1.setColor(colors[i]);
-            c.drawRoundRect(rectF1, 10.0f, 10.0f, p1);
+            //사각형 하나, 곡률, 곡률, 색상으로 둥근 직사각형 그린다.
+            c.drawRoundRect(rectF1,10.0f,10.0f,p1);
+            //
             c.drawBitmap(bitmapDrawable.getBitmap(), null, rectF1, null);
+            //
             mBlock[i] = bm;
         }
 
@@ -83,45 +88,58 @@ public class TetrisView extends View {
 
         mMovingBlockX = x;
         mMovingBlockY = y;
-        mMovingSahpe = shape;
+        mMovingShape = shape;
+    }
+
+    public int[][] getmMovingShape() {
+        return mMovingShape;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        //mGameBoard가 아예 없다면 실행되는 코드
         if (mGameBoard == null || mGameBoard.length == 0 || mGameBoard[0].length == 0) {
-            if (isInEditMode()) {
+            if (isInEditMode()) { //isInEditMode()==false
                 mGameBoard = new int[18][12];
                 Random random = new Random();
+                //i가 왜 60이지?
                 for (int i = 0; i < 60; i++) {
+                    //mGameBoard.length == 18, mGameBoard[0].length == 12 , r < 216
                     int r = random.nextInt(mGameBoard.length * mGameBoard[0].length);
+                    // r/mGameBoard[0].length는 18 미만의 정수, r%mGameboard[0].length는 12 미만의 정수값으로 나옴.
                     mGameBoard[r / mGameBoard[0].length][r % mGameBoard[0].length] = random.nextInt(7) + 1;
+
                 }
+                //mGameBoard[5][i]는 뭐지
                 for (int i = 0; i < mGameBoard[0].length; i++) {
                     mGameBoard[5][i] = random.nextInt(7) + 1;
                 }
-
+                //이건 왜 이렇게 설정된 걸까요..
                 mMovingBlockX = 2;
                 mMovingBlockY = 13;
-                mMovingSahpe = new int[][]{
+                mMovingShape = new int[][]{
                         {0, 1, 0},
                         {1, 1, 1}
                 };
             } else {
                 return;
             }
-
         }
+
+
         int w = canvas.getWidth();
         int h = canvas.getHeight();
 
 
         float borderWidth = 3 * density;
 
-        int cx = mGameBoard[0].length;
-        int cy = mGameBoard.length;
+        int cx = mGameBoard[0].length; //보드 가로 블럭갯수
+        int cy = mGameBoard.length; // 보드 세로 블럭갯수
 
+        //
         int blockSize = (int) Math.min((h - borderWidth) / cy, (w - borderWidth * 2) / cx);
 
+        //좌표
         float offsetY = borderWidth;
         float offsetX = (w - cx * blockSize) / 2;
         float bw_2 = borderWidth / 2;
@@ -129,12 +147,15 @@ public class TetrisView extends View {
         float bl = offsetX - bw_2;
         float br = offsetX + cx * blockSize + bw_2;
 
+        //테두리 선 그리기
         canvas.drawLine(bl, 0, bl, h, paintBorder);
         canvas.drawLine(bl, h - bw_2, br, h - bw_2, paintBorder);
         canvas.drawLine(br, 0, br, h, paintBorder);
 
+        //블록
         for (int y = 0; y < cy; y++) {
             for (int x = 0; x < cx; x++) {
+                //rectF.set(l,t,r,b);
                 rectF.set(offsetX + x * blockSize, h - offsetY - blockSize * (y + 1), offsetX + (x + 1) * blockSize, h - offsetY - blockSize * y);
                 int blockType = mGameBoard[y][x];
                 if (blockType > 0 && blockType <= 7) {
@@ -143,13 +164,13 @@ public class TetrisView extends View {
             }
         }
 
-        if (mMovingSahpe != null) {
-            for (int y = 0; y < mMovingSahpe.length; y++) {
-                for (int x = 0; x < mMovingSahpe[0].length; x++) {
+        if (mMovingShape != null) {
+            for (int y = 0; y < mMovingShape.length; y++) {
+                for (int x = 0; x < mMovingShape[0].length; x++) {
                     int x2 = x + mMovingBlockX;
                     int y2 = y + mMovingBlockY;
                     rectF.set(offsetX + x2 * blockSize, h - offsetY - blockSize * (y2 + 1), offsetX + (x2 + 1) * blockSize, h - offsetY - blockSize * y2);
-                    int blockType = mMovingSahpe[y][x];
+                    int blockType = mMovingShape[y][x];
                     if (blockType > 0 && blockType <= 7) {
                         canvas.drawBitmap(mBlock[blockType - 1], null, rectF, null);
                     }
