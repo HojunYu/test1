c3ac4da53c7bd865013a46ab8a89ac13e8c6cf75
seoYee <lee.seoyee9011@gmail.com>
2017-02-20 16:52:45 +0900

    Add clearLine( ) & background image

--- app/src/main/java/com/test/testris/MainActivity.java
+++ app/src/main/java/com/test/testris/MainActivity.java
@@ -4,114 +4,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static final int[][] iBlock1 = new int[][]{
+    private static final int[][] iBlock = new int[][]{
 
-            {1, 1, 1, 1},
-            {0, 0, 0, 0}
-    };
-    private int[][] iBlock2 = new int[][]{
-            {1, 0},
-            {1, 0},
-            {1, 0},
-            {1, 0}
-
+            {1, 1, 1, 1}
     };
 
-    private int[][] zBlock1 = new int[][]{
+    private int[][] zBlock = new int[][]{
             {2, 2, 0},
             {0, 2, 2}
     };
 
-    private int[][] zBlock2 = new int[][]{
-            {0, 2},
-            {2, 2},
-            {2, 0}
-    };
-
-    private int[][] sBlock1 = new int[][]{
+    private int[][] sBlock = new int[][]{
             {0, 3, 3},
             {3, 3, 0}
     };
 
-    private int[][] sBlock2 = new int[][]{
-            {3, 0},
-            {3, 3},
-            {0, 3}
-    };
-
-    private int[][] lBlock1 = new int[][]{
+    private int[][] lBlock = new int[][]{
             {0, 0, 4},
             {4, 4, 4}
     };
 
-    private int[][] lBlock2 = new int[][]{
-            {4, 4},
-            {0, 4},
-            {0, 4}
-    };
-
-    private int[][] lBlock3 = new int[][]{
-            {4, 4, 4},
-            {4, 0, 0}
-    };
-
-    private int[][] lBlock4 = new int[][]{
-            {4, 0},
-            {4, 0},
-            {4, 4}
-    };
-
-    private int[][] jBlock1 = new int[][]{
+    private int[][] jBlock = new int[][]{
             {5, 0, 0},
             {5, 5, 5}
     };
 
-    private int[][] jBlock2 = new int[][]{
-            {0, 5},
-            {0, 5},
-            {5, 5}
-    };
-
-    private int[][] jBlock3 = new int[][]{
-            {5, 5, 5},
-            {0, 0, 5}
-    };
-
-    private int[][] jBlock4 = new int[][]{
-            {5, 5},
-            {5, 0},
-            {5, 0}
-    };
-
-    private int[][] tBlock1 = new int[][]{
+    private int[][] tBlock = new int[][]{
             {6, 6, 6},
             {0, 6, 0}
-    };
-
-    private int[][] tBlock2 = new int[][]{
-            {6, 0},
-            {6, 6},
-            {6, 0}
-    };
-
-    private int[][] tBlock3 = new int[][]{
-            {0, 6, 0},
-            {6, 6, 6}
-    };
-
-    private int[][] tBlock4 = new int[][]{
-            {0, 6},
-            {6, 6},
-            {0, 6}
     };
 
     private int[][] oBlock = new int[][]{
@@ -120,14 +46,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     };
 
     private int[][][] blocks = {
-            iBlock1,
-            sBlock1,
-            jBlock1,
-            lBlock1,
-            tBlock1,
+            iBlock,
+            sBlock,
+            jBlock,
+            lBlock,
+            tBlock,
             oBlock,
-            zBlock1,
-            zBlock2
+            zBlock,
     };
 
     private int[][] mGameBoard = new int[18][12]; //18행 12열짜리 이중배열 0으로 초기화되어 있음
@@ -167,48 +92,70 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private boolean checkBlow() {
+    private boolean checkMove(int row, int col, boolean rotate) {
         boolean check = true;
-        for (int i = 0; i < curBlock[0].length; i++) {
-            if (curBlock[0][i] * mGameBoard[row - 1][col + i] != 0) {
-                check = false;
-            }
-        }
-        return check;
-    }
 
-    private boolean checkLeft() {
-        boolean check = true;
-        for (int i = 0; i < curBlock.length; i++) {
-                if (curBlock[i][0] * mGameBoard[row+i][col] != 0) {
-                    check = false;
+        int[][] block = curBlock;
+
+
+
+        if( row < 0) {
+            check = false;
+
+        }
+        if(col + block[0].length > mGameBoard[0].length  ) {
+            check =  false;
+        }
+
+        if (rotate) {
+            block = rotateBlock(block);
+        }
+
+        for (int i = 0; i < block.length; i++) {
+            if (i + row < mGameBoard.length && i+row >= 0) {
+                for (int j = 0; j < block[i].length; j++) {
+                    if (block[i][j] * mGameBoard[row + i][col + j] != 0) {
+                        check = false;
+                    }
                 }
-        }
-        return check;
-    }
-
-    private boolean checkRight(){
-        boolean check = true;
-        for(int i = 0; i < curBlock.length; i++){
-            if(curBlock[i][curBlock[0].length-1] * mGameBoard[row][col+curBlock[0].length] !=0){
-                check = false;
             }
         }
         return check;
     }
+
+    private int[][] rotateBlock(int[][] block) {
+        int[][] newBlock = new int[block[0].length][block.length];
+
+        for (int y = 0; y < newBlock.length; y++) {
+            for (int x = 0; x < newBlock[y].length; x++) {
+                newBlock[y][x] = block[x][newBlock.length - y - 1];
+            }
+        }
+
+        if(col + newBlock[0].length <= mGameBoard[0].length){
+            return newBlock;
+        }else{
+            return block;
+        }
+    }
+
+
 
     private void onTimer() {
         if (row > 0) {
-            if(checkBlow()){
+            if (checkMove(row - 1, col, false)) {
                 row--;
-            }else{
+
+            } else {
                 stitchBlock();
+                clearLine();
                 setRandomBlock();
                 row = mGameBoard.length;
                 col = 4;
             }
-        }else{
+        } else {
             stitchBlock();
+            clearLine();
             setRandomBlock();
             row = mGameBoard.length;
             col = 4;
@@ -232,6 +179,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         curBlock = null;
     }
+
+    private void clearLine(){
+        int count = 0;
+        int score = 0;
+        for(int i = 0; i< mGameBoard.length; i++){
+
+            for (int j = 0; j < mGameBoard[i].length; j++) {
+                if (mGameBoard[i][j] != 0) {
+                    count++;
+                }
+            }
+            int k;
+            if(count == 12 ){
+                for(k = i+1; k < mGameBoard.length ; k++) {
+                    for (int j = 0; j < mGameBoard[i].length; j++) {
+                        mGameBoard[k-1][j] = mGameBoard[k][j];
+                        count = 0;
+                        score ++;
+                    }
+                }
+            }else{
+                count = 0;
+            }
+        }
+    }
+
 
     //
     //***************************//
@@ -265,19 +238,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()) {
             case R.id.buttonL: //Left
                 if (col > 0) {
-                    if(checkLeft()){
+                    if (checkMove(row, col - 1, false)) {
                         col--;
                         tetrisView.setMoveBlock(col, row, curBlock);
-                    }else{
+                    } else {
                         tetrisView.setMoveBlock(col, row, curBlock);
                     }
                 }
                 break;
             case R.id.buttonR: //Right
                 if (col + curBlock[0].length < 12) {
-                    if (checkRight()) {
+                    if (checkMove(row, col + 1, false)) {
                         col++;
-                        tetrisView.setMoveBlock(col,row, curBlock);
+                        tetrisView.setMoveBlock(col, row, curBlock);
                     } else {
                         tetrisView.setMoveBlock(col, row, curBlock);
                     }
@@ -285,21 +258,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.buttonU: //Up
+                if (checkMove(row, col, true)) {
+                    curBlock = rotateBlock(curBlock);
+                    if(col + curBlock[0].length>mGameBoard[0].length){
+                        col = col-(col+curBlock[0].length - mGameBoard[0].length);
+                    }
+                    tetrisView.setMoveBlock(col, row, curBlock);
+                }
 ///////////////////////////////////////////
                 break;
 
             case R.id.buttonD: //Down
-                tetrisView.setMoveBlock(5, 13, new int[][]{
-                        {3, 3, 0},
-                        {0, 3, 3}
-                });
+                if (row > 0) {
+                    if (checkMove(row - 1, col, false)) {
+                        row--;
+                        tetrisView.setMoveBlock(col, row, curBlock);
+                    } else {
+                        tetrisView.setMoveBlock(col, row, curBlock);
+                    }
+                } else {
+                    stitchBlock();
+                    clearLine();
+                    setRandomBlock();
+                    row = mGameBoard.length;
+                    col = 4;
+                }
                 break;
 
             case R.id.buttonEnter:
-                if (mTimer != null) {
-                    onPause();
+                if (row > 0) {
+                    int r = row;
+                    while (checkMove(--r, col, false) == true) ;
+                    row = r + 1;
                 } else {
-                    onResume();
+                    stitchBlock();
+                    clearLine();
+                    setRandomBlock();
+                    row = mGameBoard.length;
+                    col = 4;
                 }
                 break;
         }
@@ -307,11 +303,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-
     //***************************//
     // 타이머
-
-
 
 
 //    private void checkBoard(int [][] curBlock){
@@ -324,8 +317,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //    }
 
 
-
- //   private boolean checkBoard() {
+    //   private boolean checkBoard() {
 //        boolean check = true;
 //
 //            for (int i = 0; i < curBlock.length; i++) {
